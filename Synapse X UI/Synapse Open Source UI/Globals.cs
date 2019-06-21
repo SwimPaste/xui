068648715f644374e8058a6f1b7aeddc007d970c
3dsboy08 <dsboy08@outlook.com>
2019-06-21 11:47:08 -0400

    Initial release of open source UI.

---
+++ Synapse X UI/Synapse Open Source UI/Globals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sxlib.Specialized;
+
+namespace Synapse_X_UI
+{
+    public static class Globals
+    {
+        public static SxLibWPF SxLib;
+    }
+}
