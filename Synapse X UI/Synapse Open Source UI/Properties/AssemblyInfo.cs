73ee3b84fcae191365493e7fc9085c03cddb8100
3dsboy08 <dsboy08@outlook.com>
2019-06-21 12:16:52 -0400

    Add GPL notices + copyright.

--- Synapse X UI/Synapse Open Source UI/Properties/AssemblyInfo.cs
+++ Synapse X UI/Synapse Open Source UI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Synapse X UI")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Synapse G.P. 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
